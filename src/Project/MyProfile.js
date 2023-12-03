3160558ee07c75c4221ce739a12a77ce3cd98a85
Rohitashwa Pareek <rhtshwprk@Rohitashwas-MacBook-Pro.local>
2023-12-03 17:54:42 +0530

    Adde profiel

--- src/Project/MyProfile.js
+++ src/Project/MyProfile.js
@@ -11,8 +11,12 @@ const MyProfile = ({ user }) => {
       <link rel="stylesheet" href="styles.css" />
       <div className="wrapper">
         <div className="left">
-          <img src="https://i.imgur.com/cMy8V5j.png" alt="user" width={100} />
-          <h3>{user.name ? user.name : ''}</h3>
+          <img
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+            alt="user"
+            width={100}
+          />
+          <h3>SHREYAS MISHRA</h3>
         </div>
         <div className="right">
           <div className="info">
@@ -20,45 +24,26 @@ const MyProfile = ({ user }) => {
             <div className="info_data">
               <div className="data">
                 <h4>Email</h4>
-                <p>alex@gmail.com</p>
+                <p>shreyasmishra@gmail.com</p>
               </div>
               <div className="data">
                 <h4>Phone</h4>
-                <p>0001-213-998761</p>
+                <p>+91 90001 10009</p>
               </div>
             </div>
           </div>
           <div className="projects">
-            <h3>Projects</h3>
+            <h3>Academic</h3>
             <div className="projects_data">
               <div className="data">
-                <h4>Recent</h4>
-                <p>Lorem ipsum dolor sit amet.</p>
+                <h4>Subjects</h4>
+                <p>Yet to be added</p>
               </div>
               <div className="data">
-                <h4>Most Viewed</h4>
-                <p>dolor sit amet.</p>
+                <h4>CGPA</h4>
+                <p>8.85</p>
               </div>
             </div>
-          </div>
-          <div className="social_media">
-            <ul>
-              <li>
-                <a href="#">
-                  <i className="fab fa-facebook-f" />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i className="fab fa-twitter" />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i className="fab fa-instagram" />
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
